05f1470227af542dc44f769c5d5b38a881dd3a36
Anastasios Lelakis <tasos@hotmail.com>
2021-02-01 22:08:27 +0200

    add relationships

---
+++ src/main/java/emergon/dao/SalesmanDao.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emergon.dao;
+
+import emergon.entity.Customer;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author user
+ */
+public class CustomerDao extends JpaUtil<Customer>{
+    
+    public List<Customer> findAll(){
+        return super.findAll("SELECT c FROM Customer c");
+    }
+    
+    @Override
+    public Customer save(Customer customer){
+        return super.save(customer);
+    }
+    
+    public Customer find(int id){
+        return super.find(Customer.class, id);
+    }
+    
+    @Override
+    public Customer update(Customer c){
+        return super.update(c);
+    }
+    
+    public void delete(int id){
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Customer c = em.find(Customer.class, id);
+        em.remove(c);
+        em.getTransaction().commit();
+        closeEntityManager();
+    }
+    
+    
+    
+}
